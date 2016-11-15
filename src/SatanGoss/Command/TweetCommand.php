547be3fa4aa11574e569e61544ee77fbdd9278d7
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-11-15 18:25:01 -0200

    Lets do this!

---
+++ src/SatanGoss/Command/TweetCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+
+class TweetCommand extends UserCommand
+{
+    protected $name = 'tweet';
+    protected $description = 'Tweet a message :)';
+    protected $usage = '/tweet';
+    protected $version = '1.0.0';
+
+    public function execute()
+    {
+        $foo = $app['twitter']->request(
+            'https://api.twitter.com/1.1/statuses/update.json',
+            'POST',
+            [
+                'status' => 'TIME IS '.time(),
+                'possibly_sensitive' => false
+            ]
+        );
+
+        $message = $this->getMessage();
+
+        $chatId = $message->getChat()->getId();
+
+        $data = [
+            'chat_id' => $chatId,
+            'text' => print_r($foo, true)
+        ];
+
+        return \Longman\TelegramBot\Request::sendMessage($data);
+    }
+}
