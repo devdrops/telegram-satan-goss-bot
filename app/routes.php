4bf7154d51760d7ccfa5e5e00d377b7c1766236b
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-11-15 15:08:40 -0200

    Test for Heroku config vars

--- app/routes.php
+++ app/routes.php
@@ -5,9 +5,10 @@
  */
 
 $app->get('/', 'SatanGoss\Controller\IndexController::indexAction')->bind('home');
-$app->get('/bot', 'SatanGoss\Controller\IndexController::botAction')->bind('bot');
-$app->get('/setup', 'SatanGoss\Controller\IndexController::setupBotAction');
 
+$app->get('/bot', 'SatanGoss\Controller\IndexController::botAction')->bind('bot');
 $app->post('/bot', 'SatanGoss\Controller\IndexController::hiBotAction');
+
+$app->get('/setup', 'SatanGoss\Controller\IndexController::setupBotAction');
 
 $app->post('/action', 'SatanGoss\Controller\BotController::webhookAction');
--- src/SatanGoss/Controller/IndexController.php
+++ src/SatanGoss/Controller/IndexController.php
@@ -23,4 +23,9 @@ class IndexController
             var_dump($exception);
         }
     }
+
+    public function indexAction(Request $request, Application $app)
+    {
+        return new JsonResponse(['FOO' => getenv('FOO')]);
+    }
 }
