7ef6f8a6bfc3601bf756df8b026c3bdf1aa6c8cc
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-11-16 01:08:58 -0200

    oh crap time to bed

--- src/SatanGoss/Command/TweetCommand.php
+++ src/SatanGoss/Command/TweetCommand.php
@@ -11,7 +11,7 @@ class TweetCommand extends UserCommand
     protected $description = 'Tweet a given <link>';
     protected $usage = '/tweet <link>';
     protected $version = '1.0.0';
-    protected $command = '/tweet'
+    protected $command = '/tweet';
 
     public function execute()
     {
