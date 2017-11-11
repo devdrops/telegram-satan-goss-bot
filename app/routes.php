2d4cac5df1451a28b69038fdeffba69cb5a177cd
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2017-11-11 17:22:09 -0200

    WIP debugging Telegram communication for new bot

--- app/routes.php
+++ app/routes.php
@@ -5,5 +5,3 @@
  */
 
 $app->post('/action', 'SatanGoss\Controller\BotController::webhookAction');
-$app->post('/pvt/payments', 'SatanGoss\Controller\BotController::webhookAction');
-$app->get('/twitter', 'SatanGoss\Controller\TwitterController::queryAction');
--- src/SatanGoss/Controller/BotController.php
+++ src/SatanGoss/Controller/BotController.php
@@ -20,7 +20,44 @@ class BotController
 
             $message = new SendMessage();
             $message->chat_id = $update->message->chat->id;
-            $message->text = 'Time is '.microtime();
+
+            /*$query = '#phptestfest #phpsp #pagarme';
+
+            $hashtagSearch = $app['twitter']->get(
+                'search/tweets',
+                [
+                    'q' => $query,
+                    'count' => 100,
+                    'result_type' => 'recent',
+                ]
+            );
+
+            $tweets = [];
+            foreach ($hashtagSearch->statuses as $item) {
+                if (true == $request->query->get('rt', null)
+                    && (
+                        true == $item->retweeted
+                        || 0 === strpos($item->text, 'RT')
+                    )
+                ) {
+                    continue;
+                }
+
+                $filtered = new \stdClass;
+
+                $filtered->created_at = $item->created_at;
+                $filtered->text = $item->text;
+                $filtered->user = $item->user->name;
+                $filtered->userName = $item->user->screen_name;
+                $filtered->isRT = $item->retweeted;
+
+                $tweets[] = $filtered;
+            }*/
+ 
+            $message->text = print_r(
+                get_class_methods(get_class($update)),
+                true
+            );
 
             $app['telegram']->performApiRequest($message);
 
